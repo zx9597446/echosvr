9261730113f23c85ee6d8e0c2dd9f0524ec620f7
zx9597446 <zx9597446@gmail.com>
2016-05-24 10:24:13 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var addr = flag.String("addr", "addr to listen", ":11111")
+
+func main() {
+	server, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("listening on ", *addr)
+	conns := clientConns(server)
+	for {
+		go handleConn(<-conns)
+	}
+}
+
+func clientConns(listener net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if err != nil {
+				log.Println(err)
+			}
+			ch <- client
+		}
+	}()
+	return ch
+}
+
+func handleConn(client net.Conn) {
+	io.Copy(io.MultiWriter(client, os.Stdout), client)
+}
