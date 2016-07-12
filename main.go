d8e53b760ea4007eba6f181c90402c0abf461277
zx9597446 <zx9597446@gmail.com>
2016-07-12 15:14:06 +0800

    udp echo server

--- main.go
+++ main.go
@@ -12,8 +12,53 @@ var addr = flag.String("a", ":11111", "addr to listen")
 var protocol = flag.String("p", "tcp", "protocol, default tcp")
 var verbose = flag.Bool("v", false, "verbose")
 
+var sem = make(chan int, 100)
+
 func main() {
 	flag.Parse()
+	if *protocol == "tcp" {
+		tcpServer()
+	} else if *protocol == "udp" {
+		udpServer()
+	}
+}
+
+func udpServer() {
+	udpaddr, err := net.ResolveUDPAddr(*protocol, *addr)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("listening on ", udpaddr)
+	udpconn, err := net.ListenUDP(*protocol, udpaddr)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer udpconn.Close()
+	for {
+		sem <- 1
+		go udpEcho(udpconn)
+	}
+}
+
+func udpEcho(con net.PacketConn) {
+	defer func() { <-sem }()
+	buf := make([]byte, 4096)
+	nr, addr, err := con.ReadFrom(buf)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	nw, err := con.WriteTo(buf[:nr], addr)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	if nw != nr {
+		log.Printf("received %d bytes but sent %d\n", nr, nw)
+	}
+}
+
+func tcpServer() {
 	server, err := net.Listen(*protocol, *addr)
 	if err != nil {
 		log.Panicln(err)
